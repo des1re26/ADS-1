a48cd8a20c9d45ccad113c77ea7a227c29ed1e9b
des1re26 <des1re26@mail.ru>
2026-03-31 01:42:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2024 <Student>
 
 #include "alg.h"
-#include <cstdlib>
 #include <cmath>
 
 bool checkPrime(uint64_t value) {
@@ -26,7 +25,6 @@ uint64_t nPrime(uint64_t n) {
         num++;
         if (checkPrime(num)) count++;
     }
-
     return num;
 }
 
@@ -40,7 +38,7 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; i++) {
+    for (uint64_t i = 2; i < hbound; ++i) {
         if (checkPrime(i)) sum += i;
     }
     return sum;
@@ -48,7 +46,7 @@ uint64_t sumPrime(uint64_t hbound) {
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
     uint64_t count = 0;
-    for (uint64_t i = lbound; i + 2 < hbound; i++) {
+    for (uint64_t i = lbound; i + 2 < hbound; ++i) {
         if (checkPrime(i) && checkPrime(i + 2)) count++;
     }
     return count;
