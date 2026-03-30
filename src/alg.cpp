264798cada9fe4dae677c824db606c9a820ab3e1
des1re26 <des1re26@mail.ru>
2026-03-31 01:32:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,21 @@
-// Copyright 2024 <Student>
-
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include <cmath>
 
+
 bool checkPrime(uint64_t value) {
-    if (value < 2) return false;
+  if (value < 2) return false;
     if (value == 2 || value == 3) return true;
     if (value % 2 == 0 || value % 3 == 0) return false;
 
     uint64_t limit = static_cast<uint64_t>(std::sqrt(value));
     for (uint64_t i = 5; i <= limit; i += 6) {
         if (value % i == 0 || value % (i + 2) == 0) return false;
-    }
-    return true;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) return 0;
+  if (n == 0) return 0;
 
     uint64_t count = 0;
     uint64_t num = 1;
@@ -24,29 +23,27 @@ uint64_t nPrime(uint64_t n) {
     while (count < n) {
         num++;
         if (checkPrime(num)) count++;
-    }
-    return num;
+  return 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t num = value + 1;
+  uint64_t num = value + 1;
     while (!checkPrime(num)) num++;
     return num;
+  return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
+   uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) sum += i;
-    }
-    return sum;
+  return 2;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    uint64_t count = 0;
+   uint64_t count = 0;
 
     for (uint64_t i = lbound; i < hbound - 2; i++) {
         if (checkPrime(i) && checkPrime(i + 2)) count++;
-    }
-    return count;
+  return 1;
 }
